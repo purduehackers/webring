bada8cef39ceb59d51ef3a50f1d3ecfe488d9c99
Kian Kasad <kian@kasad.com>
2025-05-12 23:47:29 -0700

    Add logging implementation

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,88 @@
 use std::{
     io::{IsTerminal, stderr},
+    net::SocketAddr,
+    path::PathBuf,
     process::ExitCode,
 };
 
 use axum::{Router, routing::get};
+use clap::{Parser, ValueEnum};
 use crashlog::cargo_metadata;
 use ftail::Ftail;
 use log::{LevelFilter, error, info};
+
+/// Default log level.
+const DEFAULT_LOG_LEVEL: LevelFilterWrapper = if cfg!(debug_assertions) {
+    LevelFilterWrapper::Debug
+} else {
+    LevelFilterWrapper::Info
+};
+
+#[derive(clap::Parser, Debug, Clone)]
+#[command(version, about, long_about = None)]
+struct CliOptions {
+    /// Address/port to listen on
+    #[arg(short, long = "listen-on", default_value = "0.0.0.0:3000")]
+    listen_addr: SocketAddr,
+
+    /// Log verbosity
+    #[arg(short, long, alias = "log-level", value_enum, default_value_t = DEFAULT_LOG_LEVEL)]
+    verbosity: LevelFilterWrapper,
+
+    /// File to print logs to in addition to the console
+    #[arg(short = 'o', long)]
+    log_file: Option<PathBuf>,
+}
+
+// This type exists so clap can figure out what variants are available for the verbosity option.
+// If we use LevelFilter directly, it uses the Display and FromStr implementations, which means
+// there isn't a list of possible variants for clap to use.
+#[derive(ValueEnum, Clone, Debug, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum LevelFilterWrapper {
+    Off,
+    Trace,
+    Debug,
+    Info,
+    Warn,
+    Error,
+}
+
+impl From<LevelFilterWrapper> for LevelFilter {
+    fn from(val: LevelFilterWrapper) -> Self {
+        match val {
+            LevelFilterWrapper::Off => LevelFilter::Off,
+            LevelFilterWrapper::Trace => LevelFilter::Trace,
+            LevelFilterWrapper::Debug => LevelFilter::Debug,
+            LevelFilterWrapper::Info => LevelFilter::Info,
+            LevelFilterWrapper::Warn => LevelFilter::Warn,
+            LevelFilterWrapper::Error => LevelFilter::Error,
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> ExitCode {
     // For crash reporting
     crashlog::setup!(cargo_metadata!(), false);
 
+    let cli = CliOptions::parse();
+
     // Set up logging
-    let level_filter = LevelFilter::Debug;
-    let init_result = if stderr().is_terminal() {
-        Ftail::new().formatted_console(level_filter).init()
+    let mut logger = Ftail::new();
+    if stderr().is_terminal() {
+        logger = logger.formatted_console(cli.verbosity.into());
     } else {
-        Ftail::new().console(level_filter).init()
-    };
-    if let Err(err) = init_result {
+        logger = logger.console(cli.verbosity.into());
+    }
+    if let Some(path) = cli.log_file {
+        // TODO: Remove if/when https://github.com/tjardoo/ftail/pull/9 makes it into a release
+        let Some(path_str) = path.to_str() else {
+            eprintln!("Error: log file path must be valid UTF-8");
+            return ExitCode::FAILURE;
+        };
+        logger = logger.single_file(path_str, true, cli.verbosity.into());
+    }
+    if let Err(err) = logger.init() {
         eprintln!("Error: failed to initialize logger: {err}");
         return ExitCode::FAILURE;
     }
@@ -31,7 +93,7 @@ async fn main() -> ExitCode {
     match tokio::net::TcpListener::bind(bind_addr).await {
         // Unwrapping this is fine because it will never resolve
         Ok(listener) => {
-            info!("Listening on {bind_addr}");
+            info!("Listening on http://{bind_addr}");
             axum::serve(listener, app).await.unwrap();
         }
         Err(err) => {
