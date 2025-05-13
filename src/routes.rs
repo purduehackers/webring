b7afd30c048b421cb9327a45474c2b9e44c41f7f
Kian Kasad <kian@kasad.com>
2025-05-13 00:35:19 -0700

    Add /healthcheck endpoint

--- src/routes.rs
+++ src/routes.rs
@@ -1,7 +1,10 @@
 //! Routes and endpoints
 
-use axum::Router;
-use tower_http::services::{ServeDir, ServeFile};
+use axum::{Router, response::NoContent, routing::get};
+use tower_http::{
+    cors::{Any, CorsLayer},
+    services::{ServeDir, ServeFile},
+};
 
 use crate::CliOptions;
 
@@ -10,4 +13,8 @@ pub fn create_router(cli: &CliOptions) -> Router {
     Router::new()
         .route_service("/", ServeFile::new(cli.static_dir.join("index.html")))
         .nest_service("/static", ServeDir::new(&cli.static_dir))
+        .route(
+            "/healthcheck",
+            get(async || NoContent).layer(CorsLayer::new().allow_origin(Any)),
+        )
 }
