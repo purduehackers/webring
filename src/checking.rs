0c8b02edf6752d528326df386368d8a3df3f5983
Henry Rovnyak <h.rovnyak@proton.me>
2025-05-14 11:17:12 -0400

    Small refactoring and typedefs

--- src/checking.rs
+++ src/checking.rs
@@ -1,38 +1,53 @@
-use std::sync::atomic::{AtomicI64, Ordering};
+use std::{error::Error, fmt::Display};
 
 use axum::http::Uri;
 use chrono::Utc;
+use eyre::Report;
 use log::{error, info};
 use tokio::sync::RwLock;
 
 use crate::webring::CheckLevel;
 
-static LAST_PING: AtomicI64 = AtomicI64::new(i64::MIN);
-static PING_SUCCESSFUL: RwLock<bool> = RwLock::const_new(false);
+static ADDRESS: &str = "https://ring.purduehackers.com";
 
-async fn is_online() -> bool {
+static ONLINE_CHECK_TTL_MS: i64 = 1000;
+
+static PING_INFO: RwLock<(i64, bool)> = RwLock::const_new((i64::MIN, false));
+
+async fn server_definitely_online() {
+    let at = Utc::now().timestamp_millis();
+    let mut ping_info = PING_INFO.write().await;
     let now = Utc::now().timestamp_millis();
-    let last_ping = LAST_PING.load(Ordering::Acquire);
-    if now < last_ping + 1000 {
-        return *PING_SUCCESSFUL.read().await;
+
+    if at + ONLINE_CHECK_TTL_MS < now || ping_info.0 > at {
+        return;
     }
 
-    let mut result = PING_SUCCESSFUL.write().await;
+    *ping_info = (at, true);
+}
+
+async fn is_online() -> bool {
+    {
+        let ping_info = PING_INFO.read().await;
+        let now = Utc::now().timestamp_millis();
+        if now < ping_info.0 + 1000 {
+            return ping_info.1;
+        }
+    }
+
+    let mut ping_info = PING_INFO.write().await;
 
     let now = Utc::now().timestamp_millis();
-    let last_ping = LAST_PING.load(Ordering::Acquire);
-    if now < last_ping + 1000 {
-        return *result;
+    if now < ping_info.0 + ONLINE_CHECK_TTL_MS {
+        return ping_info.1;
     }
 
     let status = surge_ping::ping("8.8.8.8".parse().unwrap(), &[0; 8]).await;
 
-    *result = status.is_ok();
-
     let now = Utc::now().timestamp_millis();
-    LAST_PING.store(now, Ordering::Release);
+    *ping_info = (now, status.is_ok());
 
-    *result
+    ping_info.1
 }
 
 /// Checks whether a given URL passes the given check level and sends of discord messages if not.
@@ -62,16 +77,53 @@ async fn check_impl(website: &Uri, check_level: CheckLevel) -> eyre::Result<()> 
             let res = reqwest::get(website.to_string())
                 .await?
                 .error_for_status()?;
+            server_definitely_online().await;
 
-            todo!()
+            let text = res.text().await?;
+
+            match contains_link(&text) {
+                None => Ok(()),
+                Some(links) => Err(Report::new(links)),
+            }
         }
         CheckLevel::JustOnline => {
             reqwest::get(website.to_string())
                 .await?
                 .error_for_status()?;
+            server_definitely_online().await;
 
             Ok(())
         }
         CheckLevel::None => Ok(()),
     }
+}
+
+#[derive(Debug)]
+pub struct MissingLinks {
+    pub home: bool,
+    pub next: bool,
+    pub prev: bool,
+}
+
+impl Display for MissingLinks {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "The webpage is missing the following links:")?;
+        if self.home {
+            writeln!(f, "- {ADDRESS}")?;
+        }
+        if self.next {
+            writeln!(f, "- {ADDRESS}/next")?;
+        }
+        if self.prev {
+            writeln!(f, "- {ADDRESS}/prev")?;
+        }
+
+        Ok(())
+    }
+}
+
+impl Error for MissingLinks {}
+
+fn contains_link(webpage: &str) -> Option<MissingLinks> {
+    None
 }
