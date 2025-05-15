90c97ffd0e20a5f3585fc51e7c0167498a15cc1f
Henry Rovnyak <h.rovnyak@proton.me>
2025-05-15 09:37:17 -0400

    Page validation code

--- src/checking.rs
+++ src/checking.rs
@@ -1,10 +1,19 @@
-use std::{error::Error, fmt::Display};
+use std::{error::Error, fmt::Display, io::ErrorKind, sync::LazyLock};
 
-use axum::http::Uri;
+use axum::{
+    body::Bytes,
+    http::{
+        Uri,
+        uri::{Authority, PathAndQuery, Scheme},
+    },
+};
 use chrono::Utc;
 use eyre::Report;
+use futures::{Stream, StreamExt, TryStreamExt};
 use log::{error, info};
+use quick_xml::{Reader, events::Event};
 use tokio::sync::RwLock;
+use tokio_util::io::StreamReader;
 
 use crate::webring::CheckLevel;
 
@@ -79,9 +88,9 @@ async fn check_impl(website: &Uri, check_level: CheckLevel) -> eyre::Result<()> 
                 .error_for_status()?;
             server_definitely_online().await;
 
-            let text = res.text().await?;
+            let stream = res.bytes_stream();
 
-            match contains_link(&text) {
+            match contains_link(stream).await {
                 None => Ok(()),
                 Some(links) => Err(Report::new(links)),
             }
@@ -105,9 +114,34 @@ pub struct MissingLinks {
     pub prev: bool,
 }
 
+impl MissingLinks {
+    fn found_link(&mut self, link: &Uri) {
+        static LINKS: LazyLock<(Authority,)> = LazyLock::new(|| (Authority::from_static(ADDRESS),));
+
+        let (links,) = &*LINKS;
+
+        if ![None, Some(&Scheme::HTTPS), Some(&Scheme::HTTP)].contains(&link.scheme()) {
+            return;
+        }
+
+        if link.authority() != Some(links) {
+            return;
+        }
+
+        let path = link.path();
+
+        match path {
+            "/" => self.home = false,
+            "/next" => self.next = false,
+            "/prev" => self.prev = false,
+            _ => {}
+        }
+    }
+}
+
 impl Display for MissingLinks {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "The webpage is missing the following links:")?;
+        writeln!(f, "Your webpage is missing the following links:")?;
         if self.home {
             writeln!(f, "- {ADDRESS}")?;
         }
@@ -118,12 +152,73 @@ impl Display for MissingLinks {
             writeln!(f, "- {ADDRESS}/prev")?;
         }
 
-        Ok(())
+        writeln!(
+            f,
+            "\nWhat to do:
+- If your webpage is rendered client-side, ask the administrators to set the validator to only check for your site being online.
+- If you don't use anchor tags with href attributes for the links, add the attribute `data-phwebring=\"prev\"|\"home\"|\"next\"` to the link elements.
+- If you think this alert is in error, contact Henry."
+        )
     }
 }
 
 impl Error for MissingLinks {}
 
-fn contains_link(webpage: &str) -> Option<MissingLinks> {
-    None
+async fn contains_link(
+    webpage: impl Stream<Item = reqwest::Result<Bytes>> + Unpin,
+) -> Option<MissingLinks> {
+    let mut reader = Reader::from_reader(StreamReader::new(
+        webpage.map_err(|e| std::io::Error::new(ErrorKind::Other, e)),
+    ));
+
+    let decoder = reader.decoder();
+
+    let mut missing_links = MissingLinks {
+        home: true,
+        next: true,
+        prev: true,
+    };
+
+    let mut buf = Vec::new();
+
+    while let Ok(event) = reader.read_event_into_async(&mut buf).await {
+        if let Event::Start(tag) = event {
+            if tag.name().0 == b"a" {
+                let Ok(Some(attr)) = tag.try_get_attribute("href") else {
+                    continue;
+                };
+
+                let Ok(attr_value) = attr.decode_and_unescape_value(decoder) else {
+                    continue;
+                };
+
+                let Ok(uri) = attr_value.parse::<Uri>() else {
+                    continue;
+                };
+
+                missing_links.found_link(&uri);
+            }
+
+            let Ok(Some(attr)) = tag.try_get_attribute("data-phwebring") else {
+                continue;
+            };
+
+            let Ok(attr_value) = attr.decode_and_unescape_value(decoder) else {
+                continue;
+            };
+
+            match &*attr_value {
+                "prev" => missing_links.prev = false,
+                "home" => missing_links.home = false,
+                "next" => missing_links.next = false,
+                _ => {}
+            }
+        }
+
+        if !missing_links.home && !missing_links.next && !missing_links.prev {
+            return None;
+        }
+    }
+
+    Some(missing_links)
 }
