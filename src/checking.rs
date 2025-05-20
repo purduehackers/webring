1c518877d8252997e5d86a1ca05dad5c375ea9c4
Henry Rovnyak <h.rovnyak@proton.me>
2025-05-20 09:45:31 -0400

    Add comments

--- src/checking.rs
+++ src/checking.rs
@@ -18,8 +18,10 @@ static ADDRESS: &str = "https://ring.purduehackers.com";
 
 static ONLINE_CHECK_TTL_MS: i64 = 1000;
 
+// (Last pinged, Was ping successful) — Used to check if the server is online
 static PING_INFO: RwLock<(i64, bool)> = RwLock::const_new((i64::MIN, false));
 
+/// If a request succeeds, then call this function to mark the server as definitely online.
 async fn server_definitely_online() {
     let at = Utc::now().timestamp_millis();
     let mut ping_info = PING_INFO.write().await;
@@ -32,8 +34,10 @@ async fn server_definitely_online() {
     *ping_info = (at, true);
 }
 
+/// Check if the server is online by either getting a cached value (cached for `ONLINE_CHECK_TTL_MS`), or by pinging `8.8.8.8`.
 async fn is_online() -> bool {
     {
+        // Has it been checked within the TTL?
         let ping_info = PING_INFO.read().await;
         let now = Utc::now().timestamp_millis();
         if now < ping_info.0 + 1000 {
@@ -41,24 +45,28 @@ async fn is_online() -> bool {
         }
     }
 
+    // Make sure that we hold the lock so that other threads wait while we're pinging instead of pinging more
     let mut ping_info = PING_INFO.write().await;
 
+    // What if another thread did the ping while we were waiting for the write lock? If so, return it.
     let now = Utc::now().timestamp_millis();
     if now < ping_info.0 + ONLINE_CHECK_TTL_MS {
         return ping_info.1;
     }
 
+    // Ping something
     let status = surge_ping::ping("8.8.8.8".parse().unwrap(), &[0; 8]).await;
 
+    // Write the info
     let now = Utc::now().timestamp_millis();
     *ping_info = (now, status.is_ok());
 
     ping_info.1
 }
 
-/// Checks whether a given URL passes the given check level and sends of discord messages if not.
+/// Checks whether a given URL passes the given check level and sends off discord messages if not.
 ///
-/// Returns Some with the result, or None if a connectivity issue on the server side is detected.
+/// Returns Some with the result, or None if the server seems to be not connected to the internet.
 #[allow(clippy::unused_async)]
 pub async fn check(website: &Uri, check_level: CheckLevel) -> Option<bool> {
     match check_impl(website, check_level).await {
@@ -85,8 +93,10 @@ async fn check_impl(website: &Uri, check_level: CheckLevel) -> eyre::Result<()> 
                 .error_for_status()?;
             server_definitely_online().await;
 
+            // Stream the body instead of collecting it into memory
             let stream = res.bytes_stream();
 
+            // Adapt the stream type returned by reqwest to the type expected by quick_xml
             match contains_link(StreamReader::new(
                 stream.map_err(|e| std::io::Error::new(ErrorKind::Other, e)),
             ))
@@ -116,6 +126,7 @@ pub struct MissingLinks {
 }
 
 impl MissingLinks {
+    /// Should be called for every link on the page. If the inputted link matches any of the expected links, mark it as found.
     fn found_link(&mut self, link: &Uri) {
         static AUTHORITY: LazyLock<Authority> =
             LazyLock::new(|| Uri::from_static(ADDRESS).into_parts().authority.unwrap());
@@ -156,7 +167,7 @@ impl Display for MissingLinks {
             f,
             "\nWhat to do:
 - If your webpage is rendered client-side, ask the administrators to set the validator to only check for your site being online.
-- If you don't use anchor tags with href attributes for the links, add the attribute `data-phwebring=\"prev\"|\"home\"|\"next\"` to the link elements.
+- If you don't use anchor tags for the links, add the attribute `data-phwebring=\"prev\"|\"home\"|\"next\"` to the link elements.
 - If you think this alert is in error, contact Henry."
         )
     }
@@ -165,6 +176,7 @@ impl Display for MissingLinks {
 impl Error for MissingLinks {}
 
 async fn contains_link(webpage: impl tokio::io::AsyncBufRead + Unpin) -> Option<MissingLinks> {
+    // Streams HTML tokens
     let mut reader = Reader::from_reader(webpage);
 
     let decoder = reader.decoder();
@@ -178,12 +190,16 @@ async fn contains_link(webpage: impl tokio::io::AsyncBufRead + Unpin) -> Option<
     let mut buf = Vec::new();
 
     while let Ok(event) = reader.read_event_into_async(&mut buf).await {
+        // If we don't break, the reader will hang
         if let Event::Eof = event {
             break;
         }
 
+        // Is the token a start tag?
         if let Event::Start(tag) = event {
+            // Is the tag an `<a ...>` tag?
             if tag.name().0 == b"a" {
+                // Try to get the href attribute
                 let Ok(Some(attr)) = tag.try_get_attribute("href") else {
                     continue;
                 };
@@ -196,25 +212,29 @@ async fn contains_link(webpage: impl tokio::io::AsyncBufRead + Unpin) -> Option<
                     continue;
                 };
 
+                // Mark the link as found if it's one we expect
                 missing_links.found_link(&uri);
-            }
+            } else {
+                // Try to get the `data-phwebring` attribute out
+                let Ok(Some(attr)) = tag.try_get_attribute("data-phwebring") else {
+                    continue;
+                };
 
-            let Ok(Some(attr)) = tag.try_get_attribute("data-phwebring") else {
-                continue;
-            };
+                let Ok(attr_value) = attr.decode_and_unescape_value(decoder) else {
+                    continue;
+                };
 
-            let Ok(attr_value) = attr.decode_and_unescape_value(decoder) else {
-                continue;
-            };
-
-            match &*attr_value {
-                "prev" => missing_links.prev = false,
-                "home" => missing_links.home = false,
-                "next" => missing_links.next = false,
-                _ => {}
+                // If the value matches any of these, mark the link as found.
+                match &*attr_value {
+                    "prev" => missing_links.prev = false,
+                    "home" => missing_links.home = false,
+                    "next" => missing_links.next = false,
+                    _ => {}
+                }
             }
         }
 
+        // If we've found all of the links, short circuit
         if !missing_links.home && !missing_links.next && !missing_links.prev {
             return None;
         }
