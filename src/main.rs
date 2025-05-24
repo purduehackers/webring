b89e41fdbda9ca6c77f96d279fee0b83c614403a
Kian Kasad <kian@kasad.com>
2025-05-23 20:36:38 -0700

    Fix missing connection info

--- src/main.rs
+++ src/main.rs
@@ -190,7 +190,9 @@ async fn main() -> ExitCode {
     log::info!("Watching {} for changes", cli.members_file.display());
 
     // Start server
-    let router = create_router(&cli.static_dir).with_state(Arc::clone(&webring));
+    let router = create_router(&cli.static_dir)
+        .with_state(Arc::clone(&webring))
+        .into_make_service_with_connect_info::<SocketAddr>();
     let bind_addr = &cli.listen_addr;
     match tokio::net::TcpListener::bind(bind_addr).await {
         // Unwrapping this is fine because it will never resolve
