d773dc3fe216695fccfc2932f49c51444947600a
Kian Kasad <kian@kasad.com>
2025-06-01 12:51:14 -0700

    Fix launch.sh
    - Changes into ~/webring first
    - Passes arguments along to script being invoked

--- ci/vulcan/launch.sh
+++ ci/vulcan/launch.sh
@@ -4,18 +4,23 @@
 # Runs one of the CI scripts based on the command provided to SSH
 #
 
+set -e
+
+cd ~/webring
+
 if [ -z "${SSH_ORIGINAL_COMMAND-x}" ]
 then
     echo '$SSH_ORIGINAL_COMMAND is not set' >&2
     exit 1
 fi
 
-case "$SSH_ORIGINAL_COMMAND" in
+read -r cmd args <<< "$SSH_ORIGINAL_COMMAND"
+case "$cmd" in
     build-for-ci | cleanup | test | deploy )
-        exec ./$SSH_ORIGINAL_COMMAND
+        exec "./ci/vulcan/${cmd}.sh" $args
         ;;
     *)
-        printf 'Invalid command `%s`\n' "$SSH_ORIGINAL_COMMAND" >&2
+        printf 'Invalid command `%s`\n' "$cmd" >&2
         exit 1
         ;;
 esac
