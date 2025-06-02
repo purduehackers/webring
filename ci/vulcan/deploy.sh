bb781fc1650d209508af59a48032659925ed5b1d
Kian Kasad <kian@kasad.com>
2025-06-01 18:49:59 -0700

    Update deploy scripts' git commands

--- ci/vulcan/deploy.sh
+++ ci/vulcan/deploy.sh
@@ -5,15 +5,15 @@
 set -e
 
 if [ $# -ne 1 ]; then
-  echo Expected one argument for the commit hash
+  echo Error: expected one argument for the commit hash >&2
   exit 1
 fi
 
 HASH=$1
 
 # Fetch and checkout the commit
-git fetch --all
-git checkout $HASH
+git fetch origin "$HASH"
+git -c advice.detachedHead=false checkout --force "$HASH"
 
 # Reload home manager — Automatically rebuilds the webring and restarts the systemd unit.
 home-manager switch
