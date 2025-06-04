0f4e2fd43b004870c891b74893bfc16fabc065a0
Henry Rovnyak <h.rovnyak@proton.me>
2025-06-04 17:28:39 -0400

    Move everything into the flake

--- flake.nix
+++ flake.nix
@@ -7,15 +7,45 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # We want to use packages from the binary cache
     flake-utils.url = "github:numtide/flake-utils";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, home-manager, rust-overlay }:
   flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
-      pkgs = nixpkgs.legacyPackages.${system};
-      gitignoreSrc = pkgs.callPackage inputs.gitignore { };
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ (import rust-overlay) ];
+      };
+
+      rust = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+        extensions = [ "rust-src" "clippy" "rustfmt" "llvm-tools-preview" ];
+      };
+
       cargo = builtins.fromTOML ( builtins.readFile ./Cargo.toml );
     in rec {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          rust
+        ] ++ (with pkgs; [
+          pkg-config
+          rust-analyzer
+          sccache
+          openssl.dev
+        ]);
+
+        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig"; 
+        RUST_BACKTRACE = 1;
+        RUSTC_WRAPPER = "sccache";
+      };
+    
       packages.phwebring = pkgs.rustPlatform.buildRustPackage rec {
         pname = cargo.package.name;
         version = cargo.package.version;
@@ -40,11 +70,54 @@
         doCheck = false;
       };
 
-      packages.default-static = ./static;
-
       legacyPackages = packages;
 
       defaultPackage = packages.phwebring;
+
+      homeConfigurations."ring" = let
+        home-manager = {
+          url = "github:nix-community/home-manager";
+          inputs.nixpkgs.follows = "nixpkgs";
+        };
+      in home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        home.username = "ring";
+        home.homeDirectory = "/home/ring";
+        home.stateVersion = "24.11";
+
+        home.packages = [ packages.phwebring ];
+
+        home.file."webring-data/static".source = ./static;
+
+        # This will automatically get restarted when rebuilding the home directory
+        systemd.user.services.phwebring = {
+          Unit.Description = "Purdue Hackers webring";
+
+          Service = {
+            ExecStart = "${pkgs.bubblewrap}/bin/bwrap \
+              --bind /home/ring/webring-data /webring \
+              --ro-bind /nix/store /nix/store \
+              --ro-bind /etc /etc \
+              --tmpfs /tmp \
+              --unshare-all \
+              --share-net \
+              --new-session \
+              --chdir /webring \
+              --uid 256 \
+              --gid 512 \
+              --die-with-parent \
+              ${packages.phwebring}/bin/ph-webring";
+
+            Restart = "on-failure";
+            Type = "exec";
+          };
+
+          Install = {
+            WantedBy = [ "default.target" ];
+          };
+        };
+      };
     }
   );
 }
