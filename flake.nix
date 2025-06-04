3134e6ae4f60354afa18c14864066834a3d10f8b
Henry Rovnyak <h.rovnyak@proton.me>
2025-06-04 18:24:50 -0400

    Use nixfmt

--- flake.nix
+++ flake.nix
@@ -17,92 +17,113 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, home-manager, rust-overlay }:
-  flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ (import rust-overlay) ];
-      };
-
-      rust = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
-        extensions = [ "rust-src" "clippy" "rustfmt" "llvm-tools-preview" ];
-      };
-
-      cargo = builtins.fromTOML ( builtins.readFile ./Cargo.toml );
-    in rec {
-      devShell = pkgs.mkShell {
-        buildInputs = [
-          rust
-        ] ++ (with pkgs; [
-          pkg-config
-          rust-analyzer
-          sccache
-          openssl.dev
-        ]);
-
-        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig"; 
-        RUST_BACKTRACE = 1;
-        RUSTC_WRAPPER = "sccache";
-      };
-    
-      packages.phwebring = pkgs.rustPlatform.buildRustPackage rec {
-        pname = cargo.package.name;
-        version = cargo.package.version;
-
-        src = ./.;
-
-        nativeBuildInputs = [ pkgs.pkg-config pkgs.openssl.dev ];
-        cargoLock = {
-          lockFile = ./Cargo.lock;
-          allowBuiltinFetchGit = true;
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      home-manager,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
         };
 
-        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-
-        meta = with pkgs.lib; {
-          homepage = cargo.package.homepage;
-          description = cargo.package.description;
-          # TODO: Change
-          license = licenses.mit;
+        rust = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+          extensions = [
+            "rust-src"
+            "clippy"
+            "rustfmt"
+            "llvm-tools-preview"
+          ];
         };
 
-        doCheck = false;
-      };
+        cargo = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          buildInputs =
+            [
+              rust
+            ]
+            ++ (with pkgs; [
+              pkg-config
+              rust-analyzer
+              sccache
+              openssl.dev
+            ]);
 
-      legacyPackages = packages;
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+          RUST_BACKTRACE = 1;
+          RUSTC_WRAPPER = "sccache";
+        };
 
-      defaultPackage = packages.phwebring;
+        packages.phwebring = pkgs.rustPlatform.buildRustPackage {
+          pname = cargo.package.name;
+          version = cargo.package.version;
 
-      packages.homeConfigurations."ring" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+          src = ./.;
 
-        modules = [ {
-          home.username = "ring";
-          home.homeDirectory = "/home/ring";
-          home.stateVersion = "24.11";
-
-          home.packages = [ packages.phwebring ];
-
-          home.file."webring-data/static".source = ./static;
-
-          # This will automatically get restarted when rebuilding the home directory
-          systemd.user.services.phwebring = {
-            Unit.Description = "Purdue Hackers webring";
-
-            Service = {
-              ExecStart = "${pkgs.bubblewrap}/bin/bwrap --bind /home/ring/webring-data /webring --ro-bind /nix/store /nix/store --ro-bind /etc /etc --tmpfs /tmp --unshare-all --share-net --new-session --chdir /webring --uid 256 --gid 512 --die-with-parent ${packages.phwebring}/bin/ph-webring";
-
-              Restart = "on-failure";
-              Type = "exec";
-            };
-
-            Install = {
-              WantedBy = [ "default.target" ];
-            };
+          nativeBuildInputs = [
+            pkgs.pkg-config
+            pkgs.openssl.dev
+          ];
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            allowBuiltinFetchGit = true;
           };
-        } ];
-      };
-    }
-  );
+
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+
+          meta = with pkgs.lib; {
+            homepage = cargo.package.homepage;
+            description = cargo.package.description;
+            # TODO: Change
+            license = licenses.mit;
+          };
+
+          doCheck = false;
+        };
+
+        legacyPackages = packages;
+
+        defaultPackage = packages.phwebring;
+
+        packages.homeConfigurations."ring" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          modules = [
+            {
+              home.username = "ring";
+              home.homeDirectory = "/home/ring";
+              home.stateVersion = "24.11";
+
+              home.packages = [ packages.phwebring ];
+
+              home.file."webring-data/static".source = ./static;
+
+              # This will automatically get restarted when rebuilding the home directory
+              systemd.user.services.phwebring = {
+                Unit.Description = "Purdue Hackers webring";
+
+                Service = {
+                  ExecStart = "${pkgs.bubblewrap}/bin/bwrap --bind /home/ring/webring-data /webring --ro-bind /nix/store /nix/store --ro-bind /etc /etc --tmpfs /tmp --unshare-all --share-net --new-session --chdir /webring --uid 256 --gid 512 --die-with-parent ${packages.phwebring}/bin/ph-webring";
+
+                  Restart = "on-failure";
+                  Type = "exec";
+                };
+
+                Install = {
+                  WantedBy = [ "default.target" ];
+                };
+              };
+            }
+          ];
+        };
+      }
+    );
 }
