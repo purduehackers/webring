07d3d295ce6cc2aecd6f05c95394901816fa5641
Henry Rovnyak <h.rovnyak@proton.me>
2025-11-01 00:58:36 -0400

    Implement clearing the statistics data daily Gitignore the webring.log
    Implement clearing the statistics data daily

--- src/stats/mod.rs
+++ src/stats/mod.rs
@@ -27,14 +27,15 @@ use std::{
     net::IpAddr,
     sync::{
         LazyLock,
-        atomic::{AtomicU64, Ordering},
+        atomic::{AtomicI32, AtomicPtr, AtomicU64, Ordering},
     },
 };
 
 use axum::http::uri::Authority;
-use chrono::{DateTime, Duration, FixedOffset, NaiveDate, Utc};
-use papaya::HashMap;
+use chrono::{DateTime, Duration, FixedOffset, Utc};
+use papaya::{Guard, HashMap};
 use sarlacc::Intern;
+use seize::Collector;
 use tracing::{info, instrument};
 
 /// The TTL for IP tracking entries, after which they are considered stale and removed.
@@ -55,22 +56,67 @@ struct IpInfo {
     started_from: Intern<Authority>,
 }
 
-#[derive(Debug, Default)]
+type Counts = HashMap<(Intern<Authority>, Intern<Authority>, Intern<Authority>), AtomicU64>;
+
+/// The counts for all of the possible webring redirects
+#[derive(Debug)]
 struct AggregatedStats {
-    /// (Date (with timezone `TIMEZONE`), From, To, Started From) → Count
-    #[expect(clippy::type_complexity)]
-    counters: HashMap<
-        (
-            NaiveDate,
-            Intern<Authority>,
-            Intern<Authority>,
-            Intern<Authority>,
-        ),
-        AtomicU64,
-    >,
+    /// The collector for the atomic data that we're handling
+    collector: Collector,
+    /// The last date that a redirect was tracked (hopefully today)
+    today: AtomicI32,
+    /// (From, To, Started From) → Count
+    /// Invariant: This MUST ALWAYS be a valid pointer
+    counter: AtomicPtr<Counts>,
 }
 
-#[derive(Debug, Default)]
+impl AggregatedStats {
+    /// Create a new `AggregatedStats`
+    fn new(now: DateTime<Utc>) -> Self {
+        let counter: Counts = HashMap::default();
+
+        AggregatedStats {
+            today: AtomicI32::new(Self::mk_num(now)),
+            counter: AtomicPtr::new(Box::into_raw(Box::new(counter))),
+            collector: Collector::new(),
+        }
+    }
+
+    /// Convert the current time into the days since the epoch
+    fn mk_num(time: DateTime<Utc>) -> i32 {
+        time.date_naive().to_epoch_days()
+    }
+
+    /// Retrieve the current counter from a guard
+    fn counter<'a>(&'a self, guard: &'a impl Guard) -> &'a Counts {
+        // SAFETY: The counter is guaranteed to be a valid pointer and we are using Acquire ordering to synchronize-with its initialization
+        unsafe { &*guard.protect(&self.counter, Ordering::Acquire) }
+    }
+
+    /// Retrieve the current counter from a guard while updating it if the current time is a new calendar date
+    fn maybe_update_counter<'a>(&'a self, now: DateTime<Utc>, guard: &'a impl Guard) -> &'a Counts {
+        let now = AggregatedStats::mk_num(now);
+
+        let prev_day = self.today.swap(now, Ordering::Relaxed);
+
+        if prev_day != now {
+            let new_counter: *mut Counts = Box::into_raw(Box::new(HashMap::new()));
+
+            // Release to synchronize-with `counter`. We don't need Acquire because we won't read the previous pointer.
+            let prev = guard.swap(&self.counter, new_counter, Ordering::Release);
+            // SAFETY: The pointer can no longer be accessed now that it has been swapped into `prev`, and `Box::from_raw` is the correct way to drop the pointer.
+            unsafe {
+                self.collector
+                    .retire(prev, |ptr, _| drop(Box::from_raw(ptr)));
+            }
+        }
+
+        self.counter(guard)
+    }
+}
+
+/// Statistics tracking for the webring
+#[derive(Debug)]
 pub struct Stats {
     /// Aggregated statistics
     aggregated: AggregatedStats,
@@ -80,9 +126,9 @@ pub struct Stats {
 
 impl Stats {
     /// Creates a new instance of `Stats`.
-    pub fn new() -> Stats {
+    pub fn new(now: DateTime<Utc>) -> Stats {
         Stats {
-            aggregated: AggregatedStats::default(),
+            aggregated: AggregatedStats::new(now),
             ip_tracking: HashMap::new(),
         }
     }
@@ -115,11 +161,9 @@ impl Stats {
             },
         );
 
-        let date = now.with_timezone(&TIMEZONE).date_naive();
-
-        let pinned_map = self.aggregated.counters.pin();
-        let counter =
-            pinned_map.get_or_insert((date, from, to, ip_info.started_from), AtomicU64::new(0));
+        let guard = self.aggregated.collector.enter();
+        let pinned_map = self.aggregated.maybe_update_counter(now, &guard).pin();
+        let counter = pinned_map.get_or_insert((from, to, ip_info.started_from), AtomicU64::new(0));
 
         counter.fetch_add(1, Ordering::Relaxed);
     }
@@ -144,17 +188,17 @@ impl Stats {
     }
 
     #[cfg(test)]
-    pub fn assert_stat_entry(&self, entry: (NaiveDate, &str, &str, &str), count: u64) {
+    pub fn assert_stat_entry(&self, entry: (&str, &str, &str), count: u64) {
+        let guard = self.aggregated.collector.enter();
         assert_eq!(
             self.aggregated
-                .counters
+                .counter(&guard)
                 .pin()
                 .get(&(
-                    entry.0,
+                    Intern::new(entry.0.parse::<Authority>().unwrap()),
                     Intern::new(entry.1.parse::<Authority>().unwrap()),
                     Intern::new(entry.2.parse::<Authority>().unwrap()),
-                    Intern::new(entry.3.parse::<Authority>().unwrap()),
-                ))
+                ),)
                 .map_or(0, |v| v.load(Ordering::Relaxed)),
             count,
             "{self:#?}\n{entry:?}"
@@ -192,7 +236,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_stat_tracking() {
-        let stats = Stats::new();
+        let stats = Stats::new(t(0));
 
         stats.redirected_impl(a("0.0.0.0"), i("a.com"), i("b.com"), t(0));
         stats.redirected_impl(a("0.0.0.0"), i("b.com"), i("c.com"), t(1));
@@ -201,11 +245,12 @@ mod tests {
         stats.redirected_impl(a("1.0.0.0"), i("b.com"), i("homepage.com"), t(2));
         stats.redirected_impl(a("1.0.0.0"), i("homepage.com"), i("c.com"), t(3));
 
-        assert_eq!(stats.aggregated.counters.len(), 4);
-        stats.assert_stat_entry((d(0), "a.com", "b.com", "a.com"), 2);
-        stats.assert_stat_entry((d(0), "b.com", "c.com", "a.com"), 1);
-        stats.assert_stat_entry((d(0), "b.com", "homepage.com", "a.com"), 1);
-        stats.assert_stat_entry((d(0), "homepage.com", "c.com", "a.com"), 1);
+        let guard = stats.aggregated.collector.enter();
+        assert_eq!(stats.aggregated.counter(&guard).len(), 4);
+        stats.assert_stat_entry(("a.com", "b.com", "a.com"), 2);
+        stats.assert_stat_entry(("b.com", "c.com", "a.com"), 1);
+        stats.assert_stat_entry(("b.com", "homepage.com", "a.com"), 1);
+        stats.assert_stat_entry(("homepage.com", "c.com", "a.com"), 1);
 
         let tracking = stats.ip_tracking.pin();
         assert_eq!(tracking.len(), 2);
@@ -243,13 +288,8 @@ mod tests {
         stats.redirected_impl(a("0.0.0.0"), i("b.com"), i("c.com"), t(1) + day);
         stats.redirected_impl(a("0.0.0.0"), i("c.com"), i("a.com"), t(2) + day);
 
-        assert_eq!(stats.aggregated.counters.len(), 6);
-        stats.assert_stat_entry((d(0), "a.com", "b.com", "a.com"), 2);
-        stats.assert_stat_entry((d(0), "b.com", "c.com", "a.com"), 1);
-        stats.assert_stat_entry((d(0), "b.com", "homepage.com", "a.com"), 1);
-        stats.assert_stat_entry((d(0), "homepage.com", "c.com", "a.com"), 1);
-        stats.assert_stat_entry((d(day.num_seconds()), "b.com", "c.com", "b.com"), 1);
-        stats.assert_stat_entry((d(day.num_seconds()), "b.com", "c.com", "b.com"), 1);
-        stats.assert_stat_entry((d(day.num_seconds()), "c.com", "a.com", "b.com"), 1);
+        assert_eq!(stats.aggregated.counter(&guard).len(), 2);
+        stats.assert_stat_entry(("b.com", "c.com", "b.com"), 1);
+        stats.assert_stat_entry(("c.com", "a.com", "b.com"), 1);
     }
 }
--- src/webring.rs
+++ src/webring.rs
@@ -29,7 +29,7 @@ use std::{
 };
 
 use axum::http::{Uri, uri::Authority};
-use chrono::TimeDelta;
+use chrono::{TimeDelta, Utc};
 use futures::{StreamExt, future::join, stream::FuturesUnordered};
 use indexmap::IndexMap;
 use notify::{EventKind, RecommendedWatcher, RecursiveMode, Watcher as _};
@@ -228,7 +228,7 @@ impl Webring {
             members: RwLock::new(member_map_from_config_table(&config.members)),
             static_dir_path: config.webring.static_dir.clone(),
             homepage: AsyncRwLock::new(None),
-            stats: Arc::new(Stats::new()),
+            stats: Arc::new(Stats::new(Utc::now())),
             file_watcher: OnceLock::default(),
             base_address: config.webring.base_url(),
             notifier: config
@@ -648,7 +648,7 @@ impl Webring {
     }
 
     #[cfg(test)]
-    pub fn assert_stat_entry(&self, entry: (chrono::NaiveDate, &str, &str, &str), count: u64) {
+    pub fn assert_stat_entry(&self, entry: (&str, &str, &str), count: u64) {
         self.stats.assert_stat_entry(entry, count);
     }
 }
@@ -699,7 +699,7 @@ mod tests {
     use crate::{
         config::{Config, MemberSpec},
         discord::{DiscordNotifier, NOTIFICATION_DEBOUNCE_PERIOD, Snowflake},
-        stats::{TIMEZONE, UNKNOWN_ORIGIN},
+        stats::{Stats, UNKNOWN_ORIGIN},
         webring::{CheckLevel, Webring},
     };
 
@@ -716,7 +716,7 @@ mod tests {
                 base_address: Intern::default(),
                 base_authority: Intern::new("ring.purduehackers.com".parse().unwrap()),
                 notifier: None,
-                stats: Arc::default(),
+                stats: Arc::new(Stats::new(Utc::now())),
                 config: Arc::new(AsyncRwLock::new(None)),
             }
         }
@@ -847,21 +847,11 @@ mod tests {
             assert_eq!(*inner, expected);
         }
 
-        let today = Utc::now().with_timezone(&TIMEZONE).date_naive();
-
         webring.assert_next(
             "https://hrovnyak.gitlab.io/bruh/bruh/bruh?bruh=bruh",
             Ok("kasad.com"),
         );
-        webring.assert_stat_entry(
-            (
-                today,
-                "hrovnyak.gitlab.io",
-                "kasad.com",
-                "hrovnyak.gitlab.io",
-            ),
-            1,
-        );
+        webring.assert_stat_entry(("hrovnyak.gitlab.io", "kasad.com", "hrovnyak.gitlab.io"), 1);
 
         webring.assert_prev(
             "https://hrovnyak.gitlab.io/bruh/bruh/bruh?bruh=bruh",
@@ -869,7 +859,6 @@ mod tests {
         );
         webring.assert_stat_entry(
             (
-                today,
                 "hrovnyak.gitlab.io",
                 "refuse-the-r.ing",
                 "hrovnyak.gitlab.io",
@@ -1047,15 +1036,12 @@ mod tests {
         .unwrap();
         let webring = Webring::new(&config);
 
-        let today = Utc::now().with_timezone(&TIMEZONE).date_naive();
-
         let uri = webring
             .random_page(None, "0.0.0.0".parse().unwrap())
             .unwrap();
         assert_eq!(uri, Intern::new("kasad.com".parse().unwrap()));
         webring.assert_stat_entry(
             (
-                today,
                 UNKNOWN_ORIGIN.as_str(),
                 "kasad.com",
                 UNKNOWN_ORIGIN.as_str(),
@@ -1073,8 +1059,6 @@ mod tests {
         "# }).unwrap();
         let webring = Webring::new(&config);
 
-        let today = Utc::now().with_timezone(&TIMEZONE).date_naive();
-
         let uri = webring
             .random_page(
                 Some(&"clementine.viridian.page".parse().unwrap()),
@@ -1084,7 +1068,6 @@ mod tests {
         assert_eq!(uri, Intern::new("kasad.com".parse().unwrap()));
         webring.assert_stat_entry(
             (
-                today,
                 "clementine.viridian.page",
                 "kasad.com",
                 "clementine.viridian.page",
